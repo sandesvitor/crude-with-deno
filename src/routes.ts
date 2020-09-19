b576db46fc4d4c8c72384d7c8ea8a11f495bc780
Vitor Sandes <sandesvitor@gmail.com>
2020-09-19 12:07:23 -0300

    Added get by id

--- src/routes.ts
+++ src/routes.ts
@@ -1,36 +1,50 @@
 import express, { Response, Request, Router} from 'express'
 import User from './model/User'
 
-const users:object[] = []
+const users:Array<User> = []
 let idIncrement:number = 1
 
 const routes: Router = express.Router()
 
 routes.post('/users', (req: Request, res: Response) => {
+    console.debug("############### POST REQUEST: STAR ##############")
     const id:number = idIncrement
     idIncrement++
 
     const name:string = req.body.name
     const email:string = req.body.email
     
-    const user = new User(id, name, email)
+    const user: User = new User(id, name, email)
     users.push(user)
 
     console.debug(user)
-    return res.status(200).json()
+    console.debug("############### POST REQUEST: DONE ##############\n")
+    return res.status(200).json(user)
 })
 
 routes.get('/users', (req: Request, res: Response) => {
-    res.send(JSON.stringify(users, null, 2))
-    console.log(JSON.stringify(users, null, 2))
-    return res.status(200)
+    console.debug("############### GET REQUEST: STAR ###############")
+    console.log(users)
+    console.debug("############### GET REQUEST: DONE ###############\n")
+    return res.status(200).json(users)
 })
 
 routes.get('/users/:id', (req: Request, res: Response) => {
+    console.debug("############### GET BY ID REQUEST: START ###############")
     const userId:number = parseInt(req.params.id)
+    
+    const userById:User | undefined = users.find((user:User):User | Error => {
+        if (user.id === userId){
+            return user
+        } else {
+            console.debug("############### GET BY ID REQUEST: DONE ###############\n")
+            throw new Error("User not found")
+        }
+    })
 
-    console.log(userId)
-    return res.status(200)
+    console.log(userById)
+    console.debug("############### GET BY ID REQUEST: DONE ###############\n")
+    return res.status(200).json(userById)
 })
 
 export default routes
